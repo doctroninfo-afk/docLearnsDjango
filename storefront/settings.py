f82ce26ce17d9f1ea08de5e28c1d57cf78812cc4
doctroninfo-afk <doctron.info@gmail.com>
2026-07-01 10:34:35 +0545

    Learned restful api, installed django rest framework, created api views and creating serializers

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'playground',
     'debug_toolbar',
     'store', 
@@ -82,7 +83,7 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
+        'NAME': 'storefront2',
         'HOST' : 'localhost', 
         'USER' : 'root',
         'PASSWORD' : 'iPhoneX64GB$'
